532d6ba7086fe05e0f3afbf6b91fc7d7f5a7f3df
Kevin Roberts <kroberts@citytechinc.com>
2014-09-05 11:39:47 -0500

    update to load disqus on demand

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,23 @@
 (function ($) {
   $(document).ready(function () {
     $('.post').fitVids();
+	
+	// Requires jQuery of course.
+
+	    $('.show-comments').on('click', function(){
+	          var disqus_shortname = 'thekev';
+
+	          // ajax request to load the disqus javascript
+	          $.ajax({
+	                  type: "GET",
+	                  url: "http://" + disqus_shortname + ".disqus.com/embed.js",
+	                  dataType: "script",
+	                  cache: true
+	          });
+	          // hide the button once comments load
+	          $(this).fadeOut();
+	    });
+
+	
   });
 })(jQuery);

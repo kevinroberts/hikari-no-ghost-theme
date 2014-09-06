d9e4d348a0c6cebfe0af9f66600726b170769784
Kevin Roberts <kevin.roberts10@gmail.com>
2014-09-06 18:47:26 -0500

    update disqus embed logic

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,25 @@
-(function ($) {
-  $(document).ready(function () {
-    $('.post').fitVids();
-	
-	// Requires jQuery of course.
+(function($) {
+	$(document).ready(function() {
+		$('.post').fitVids();
+		// Disqus comment embedding
+		var protocol = 'http://';
+		if (window.location.protocol === 'https:') {
+			protocol = "https://"
+		}
+		$('.show-comments').on('click', function() {
+			var disqus_shortname = 'thekev';
 
-	    $('.show-comments').on('click', function(){
-	          var disqus_shortname = 'thekev';
+			// ajax request to load the disqus javascript
+			$.ajax({
+				type: "GET",
+				url: protocol + disqus_shortname + ".disqus.com/embed.js",
+				dataType: "script",
+				cache: true
+			});
+			// hide the button once comments load
+			$(this).fadeOut();
+		});
 
-	          // ajax request to load the disqus javascript
-	          $.ajax({
-	                  type: "GET",
-	                  url: "http://" + disqus_shortname + ".disqus.com/embed.js",
-	                  dataType: "script",
-	                  cache: true
-	          });
-	          // hide the button once comments load
-	          $(this).fadeOut();
-	    });
 
-	
-  });
+	});
 })(jQuery);
